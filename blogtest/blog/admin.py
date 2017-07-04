e3b3b5c2499d503e2ea90c46de93e1cacb5ea36c
Alekxandr <alkv84@yandex.ru>
2017-07-04 23:15:49 +0300

    Изменения в админке

--- blogtest/blog/admin.py
+++ blogtest/blog/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Пост', {'fields': ['title']}),
+        (None, {'fields': ['content']}),
+        (None, {'fields': ['datetime']}),
+    ]
+    inlines = [CommentInline]
+
+
+admin.site.register(Post, PostAdmin)
